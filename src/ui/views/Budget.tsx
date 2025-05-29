fc8a1b90f1751f01eb3889fdbd2028e60d6f5583
st003 <stuart.j.calcote@gmail.com>
2025-05-28 20:17:43 -0700

    Added basic layout of dashboard and budget pages

--- src/ui/views/Budget.tsx
+++ src/ui/views/Budget.tsx
@@ -2,6 +2,9 @@ export default function Budget() {
   return (
     <>
       <h1>Budget</h1>
+      <h2>Categories</h2>
+      <hr />
+      <h2>Transactions</h2>
     </>
   )
 }
--- src/ui/views/Dashboard.tsx
+++ src/ui/views/Dashboard.tsx
@@ -1,7 +1,21 @@
+import { Grid } from "@mui/material";
+
 export default function Dashboard() {
   return (
     <>
       <h1>Dashboard</h1>
+      <Grid container>
+        <h2>Net Income (Month to Month)</h2>
+      </Grid>
+      <hr />
+      <Grid container>
+        <Grid size={{ sm: 12, lg: 6 }}>
+          <h2>Budget (Previous Month)</h2>
+        </Grid>
+        <Grid size={{ sm: 12, lg: 6 }}>
+          <h2>Net Income (Year to Year)</h2>
+        </Grid>
+      </Grid>
     </>
   )
 }
