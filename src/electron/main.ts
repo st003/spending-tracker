1f931c2978949d67c3944f489d4d096005b8a887
st003 <stuart.j.calcote@gmail.com>
2025-06-05 21:05:26 -0700

    Added platform specific window open/close behavior

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,8 +1,8 @@
-import path from 'path';
-import { app, BrowserWindow } from 'electron';
+import path from 'path'
+import { app, BrowserWindow } from 'electron'
 
-app.whenReady().then(() => {
 
+function createWindow() {
   const mainWindow = new BrowserWindow({
     // TODO: make this fill the screen on open
     width: 1280,
@@ -11,10 +11,29 @@ app.whenReady().then(() => {
 
   // when in development mode, load the window directly from Vite so we can have hot module reloading
   if (process.env.NODE_ENV === 'development') {
-    mainWindow.loadURL('http://localhost:5173');
+    mainWindow.loadURL('http://localhost:5173')
   } else {
     // path.join() used for file system compatability
     // app.getAppPath() allows the electro app to find its files no matter where it exists on the file system
-    mainWindow.loadFile(path.join(app.getAppPath(), '/dist-ui/index.html'));
+    mainWindow.loadFile(path.join(app.getAppPath(), '/dist-ui/index.html'))
   }
-});
+}
+
+// app main statup logic
+app.whenReady().then(() => {
+  createWindow()
+
+  // MacOS specific behavior for opening a window if non are open
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+// Windows/Linux behavior for closing the application once all windows are closed
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
