50b10c536f9054f003552571f7396b13c243bfc7
st003 <stuart.j.calcote@gmail.com>
2025-07-22 18:31:00 -0700

    Added automatic database initiation for packaged app

--- src/electron/db/index.ts
+++ src/electron/db/index.ts
@@ -13,16 +13,16 @@ export var DB: string
  * Initiates the database path
  */
 export async function initDatabase() {
+
   if (process.env.NODE_ENV === 'development') {
     DB = 'data.db'
-
-    if (!fs.existsSync(DB)) {
-      console.warn('Development database does not exist. Creating new one...')
-      await createNewDatabase(DB)
-    }
-
   } else {
     const userDataDir = app.getPath('userData')
     DB = `${userDataDir}/data.db`
+  }
+
+  if (!fs.existsSync(DB)) {
+    console.info('Database does not exist. Creating new one...')
+    await createNewDatabase(DB)
   }
 }
