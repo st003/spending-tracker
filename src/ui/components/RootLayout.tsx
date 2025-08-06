54648d0d47a873297d708a02d311592472190be8
st003 <stuart.j.calcote@gmail.com>
2025-08-06 16:46:36 -0700

    Moved Importer into RootLayout

--- src/ui/components/RootLayout.tsx
+++ src/ui/components/RootLayout.tsx
@@ -6,27 +6,32 @@ import BottomNavigationAction from '@mui/material/BottomNavigationAction'
 import BarChartIcon from '@mui/icons-material/BarChart'
 import PieChartIcon from '@mui/icons-material/PieChart'
 
+import Importer from './Importer'
+
 export default function RootLayout() {
 
   const navigate = useNavigate()
 
   return (
-    <div className='rootLayout'>
-      <main>
-        <Outlet />
-      </main>
-      <footer>
-        <BottomNavigation showLabels sx={{ backgroundColor: '#f6f7f8'}}>
-          <BottomNavigationAction
-            label='Dashboard'
-            icon={<BarChartIcon />}
-            onClick={() => navigate('/dashboard')} />
-          <BottomNavigationAction
-            label='Budget'
-            icon={<PieChartIcon />}
-            onClick={() => navigate('/budget')} />
-        </BottomNavigation>
-      </footer>
-    </div>
+    <>
+      <div className='rootLayout'>
+        <main>
+          <Outlet />
+        </main>
+        <footer>
+          <BottomNavigation showLabels sx={{ backgroundColor: '#f6f7f8'}}>
+            <BottomNavigationAction
+              label='Dashboard'
+              icon={<BarChartIcon />}
+              onClick={() => navigate('/dashboard')} />
+            <BottomNavigationAction
+              label='Budget'
+              icon={<PieChartIcon />}
+              onClick={() => navigate('/budget')} />
+          </BottomNavigation>
+        </footer>
+      </div>
+      <Importer />
+    </>
   )
 }
--- src/ui/main.tsx
+++ src/ui/main.tsx
@@ -3,13 +3,11 @@ import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 
 import router from './router'
-import Importer from './components/Importer'
 
 import './styles/global.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <RouterProvider router={router} />
-    <Importer />
   </StrictMode>,
 )
