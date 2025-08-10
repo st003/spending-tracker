7de56b9959177d575c113cecbc60dd8d9f0e17a1
st003 <stuart.j.calcote@gmail.com>
2025-08-10 14:33:11 -0700

    Added support for opening and parsing an import csv file

--- src/electron/import.ts
+++ src/electron/import.ts
@@ -1,6 +1,10 @@
+import fs from 'fs'
 import path from 'path'
 
+import { parse } from 'csv-parse'
+
 import { BrowserWindow, dialog } from 'electron'
+import { LargeNumberLike } from 'crypto'
 
 /**
  * Opens the file selection dialog for choosing a CSV file.
@@ -28,10 +32,33 @@ export async function selectImportFile(mainWindow: BrowserWindow) {
 }
 
 /**
+ * Parses a CSV file into an array of object. The only constraint placed upon the
+ * CSV file is the first row contains column headings. Otherwise this function sets
+ * no restrictions as to the number of columns or their contents.
+ *
+ * @param filePath The full path to the CSV file to import data from
+ * @returns A Promise with an array of objects
+ */
+function parseCSV(filePath: string): Promise<object[]> {
+  return new Promise((resolve, reject) => {
+    const rows: object[] = []
+    fs.createReadStream(filePath)
+      .pipe(parse({
+        columns: true, // import csv requires column headers
+        skip_empty_lines: true,
+        trim: true,
+      }))
+      .on('data', (row: object) => rows.push(row))
+      .on('end', () => resolve(rows))
+      .on('error', error => reject(error))
+    })
+}
+
+/**
  * Opens the import CSV, parses and validates the data, then inserts
  * the data into the database.
  *
- * @param filePath 
+ * @param filePath The full path to the CSV file to import data from
  */
 export async function importData(filePath: string) {
 
@@ -40,10 +67,15 @@ export async function importData(filePath: string) {
     throw new Error(`Selected file must have the extention '.csv'`)
   }
 
-  // TODOs
-  // open & parse csv (https://www.npmjs.com/package/csv-parse)
-  // validate data structure
-  // import data
+  try {
+    const rows = await parseCSV(filePath)
+    console.log(rows)
 
-  return
+    // TODOs
+    // validate data structure
+    // import data into database
+
+  } catch (error) {
+    throw error
+  }
 }
